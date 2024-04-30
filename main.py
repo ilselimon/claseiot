29af894b609f7c0aa9a96c5e3c16d4195a8cef94
ilselimon <168222842+ilselimon@users.noreply.github.com>
2024-04-30 13:08:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def Hola_mundo():
   return "Esta API te ayudará a sumar, restar, multiplicar y dividr"
 
 @app.route("Suma/<numerouno>/<numerodos>")
-def Multiplicacion(numerouno)(numerodos):
+def Multiplicacion(numerouno):(numerodos):
   Resultado=int(numerouno)*(numerodos)
   return f"El resultado es {Resultado}"
 
