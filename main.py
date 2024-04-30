f42fa26b564059a953a3f146e0c7082d281b0b23
ilselimon <168222842+ilselimon@users.noreply.github.com>
2024-04-30 12:58:57 -0600

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ app=Flask(__name__)
 
 @app.route("/")
 def Hola_mundo():
-  return "Hola a todos"
+  return "Esta API te ayudará a sumar, restar, multiplicar y dividr"
+
+@app.route("Suma/<numerouno>/<numerodos>")
+def Multiplicacion(numerouno)(numerodos):
+  Resultado=int(numerouno)*(numerodos)
+  return f"El resultado es {Resultado}
 
 if __name__=="__main__":
   app.run()
