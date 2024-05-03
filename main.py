7f24574e95483c80b6355588ff1df7787961f7a0
ilselimon <168222842+ilselimon@users.noreply.github.com>
2024-05-03 13:13:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,34 @@ from flask import Flask
 
 app=Flask(__name__)
 
-base=pd.read_excel("prueba.xlsx")
+base=pd.read_excel("Alumnos de Hogwarts.xlsx")
 
 @app.route("/")
 def Principal():
-    return "Esta es una Api que te muestra alumnos"
+    return "Esta es una Api que te muestra alumnos de Hogwarts"
 
-@app.route("/Por_Nombre/<Nombre>")
-def PorNombre(Nombre):
-  fila=base[base["Nombre"]==Nombre]
+@app.route("/Por_Alumno/<Alumno>")
+def PorAlumno(Alumno):
+  resultados=base[base["Alumno"]==Alumno]
   resultados=str(resultados)
   return resultados
 
-@app.route("/Por_Edad/<Edad>")
-def PorEdad(Edad):
-  Edad=int(Edad)
-  resultados=base[base["Edad"]==Edad]
+@app.route("/Por_Nacimiento/<Nacimiento>")
+def PorNacimiento(Nacimiento):
+  Nacimiento=int(Nacimiento)
+  resultados=base[base["Nacimiento"]==Nacimiento]
   resultados=str(resultados)
   return resultados
 
-@app.route("/Por_Estado/<Estado>")
-def PorEstado(Estado):
-  resultados=base[base["Estado"]==Estado]
+@app.route("/Por_Apellido/<Apellido>")
+def PorApellido(Apellido):
+  resultados=base[base["Apellido"]==Apellido]
+  resultados=str(resultados)
+  return resultados
+
+@app.route("/Por_Casa/<Casa>")
+def PorCasa(Casa):
+  resultados=base[base["Casa"]==Casa]
   resultados=str(resultados)
   return resultados
 
